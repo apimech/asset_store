0366d2b007844772ddd21b4c0db761db7eba6901
Matt F <matthew.fraser@gmail.com>
2021-03-14 18:13:45 +1300

    abcd

--- go.ps1
+++ go.ps1
@@ -1,2 +1,1 @@
-$randStr = -join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_})
-git add .;git commit -m "$randStr";git push -u origin main
+git add .;git commit -m "abcd";git push -u origin main
