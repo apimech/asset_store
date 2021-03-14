e993943ebbf52d2a4fd723e79b46d37f95129888
Matt F <matthew.fraser@gmail.com>
2021-03-14 18:56:21 +1300

    abcd

--- js/myScripts.js
+++ js/myScripts.js
@@ -71,7 +71,7 @@ jQuery._fetch = function (url, options) {
     // Allow user to set any option except for dataType, cache, and url
     options = $.extend(options || {}, {
       dataType: "text",
-      cache: true,
+      cache: false,
       url: url,
     });
     dMsg("jq ._fetch", [
