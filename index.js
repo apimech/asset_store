c3799310cb6820744815cc473e956ce9436c1006
Matt F <matthew.fraser@gmail.com>
2021-04-16 16:35:37 +1200

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.get('/:dir/:file', function (req, res) {
 });
 
 
+
 // Change the 404 message modifing the middleware
 app.use(function(req, res, next) {
     res.status(404).send("Not Found");
